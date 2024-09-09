b0e80e81119431c94bebbfec217cfaa1505a8917
Firzan Armani <mrfirzanarmani@gmail.com>
2024-09-09 11:11:17 +0800

    set database credentials dotenvx

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,4 +1,5 @@
 import { type Config } from "drizzle-kit";
+import "dotenv/config";
 
 export default {
   schema: "./src/db/schema.ts",
@@ -6,9 +7,9 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    accountId: "",
-    databaseId: "",
-    token: "",
+    accountId: "affe358d09faf0cadfa28e7e5f6d650a",
+    databaseId: "a67b942e-b48b-4483-bb4b-6c78c510c53c",
+    token: process.env.CLOUDFLARE_TOKEN ?? "",
   },
   verbose: true,
   strict: true,
